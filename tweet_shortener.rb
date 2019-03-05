24bf3c1cc0511f9aa6c26e0809c0d23aa7928289
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 16:06:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,3 +13,15 @@ def dictionary
 }
 end
 
+
+def word_substituter(tweet)
+  # replaces long words with their expected short form
+
+    tweet.split(" ").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
+    end
+  end.join(" ")
+end
