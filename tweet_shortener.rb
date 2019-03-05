ffd430ebe90b2f471ed7e741cecc6d54de1f3352
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 16:14:07 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,10 +36,10 @@ def selective_tweet_shortener(tweet)
   tweet.split(" ").map do |phrase|
     if phrase.length > 140
       word_substituter(phrase)
-    elsif phrase.length <= 140
+    elsif phrase.length <= 130
       phrase
     end
-end
+  end
 end
   
   
